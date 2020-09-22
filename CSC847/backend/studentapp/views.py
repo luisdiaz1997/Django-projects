d54d93b8577c400846c3703c052124d9c170fb76
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-09-22 01:40:19 -0700

    tutorial 4 done

--- CSC847/backend/studentapp/views.py
+++ CSC847/backend/studentapp/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'studentapp/index.html', context)
 # Create your views here.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'studentapp/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of questions %s."
