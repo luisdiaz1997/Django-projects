9b49226c6e9ef821c12e1dd25439364ac111e9ee
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-09-24 19:09:54 -0700

    fixed routes

--- CSC847/backend/backend/urls.py
+++ CSC847/backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('studentapp/', include('studentapp.urls')),
+    path('', include('studentapp.urls')),
     path('admin/', admin.site.urls),
 ]
